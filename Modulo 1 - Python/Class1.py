a56cff738a5fa8e8bee571625c95b99e06be986f
Max Jimenez <maxjip@gmail.com>
2021-06-02 00:48:18 -0500

    Creando tipos de variables

--- Modulo 1 - Python/Class1.py
+++ Modulo 1 - Python/Class1.py
@@ -2,3 +2,22 @@
 
 #Leccion 1
 
+    #Variables
+char='a' #Esto es un caracter
+print(char)
+string="Hola Mundo" #Esto es una cadena de caracteres
+print(string)
+int=5 #Esto es un entero
+print(int)
+float=2.8 #Esto es un número flotante
+print(float)
+boolean=True #Esto es un boleano
+print(boolean)
+
+
+
+my_name="Jose"
+my_surname="Garcia"
+
+print(f"{my_name} {my_surname}")
+
