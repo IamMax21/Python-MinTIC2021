689cc5bbf0d7e45a5dcb09dc2c74903554a773d9
Max Jimenez <maxjip@gmail.com>
2021-06-03 00:34:51 -0500

    Ejercicio 1 y 2 con la libreria math

---
+++ Modulo 1 - Python/Class2.py
@@ -0,0 +1,14 @@
+import math
+
+#Usando la libreria math para realizar diferentes calculos
+
+#Ejercicio 1. Calcula el coseno de un número
+number1=10
+coseno1=math.cos(number1)
+
+#Ejercicio 2. Calcula la raíz cuadrada de un número insertado por el usuario
+number2=int(input("Digite un número "))
+raiz=math.sqrt(number2)
+print(raiz)
+
+#Ejercicio 3.
