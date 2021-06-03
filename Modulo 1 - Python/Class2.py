384b41b988a4410593aecc7f15ca6dcdeadfb8e3
Max Jimenez <maxjip@gmail.com>
2021-06-03 01:06:57 -0500

    Resolviendo Ejercicio 3 y 4

--- Modulo 1 - Python/Class2.py
+++ Modulo 1 - Python/Class2.py
@@ -1,7 +1,7 @@
 import math
 
 #Usando la libreria math para realizar diferentes calculos
-
+"""
 #Ejercicio 1. Calcula el coseno de un número
 number1=10
 coseno1=math.cos(number1)
@@ -10,5 +10,15 @@ coseno1=math.cos(number1)
 number2=int(input("Digite un número "))
 raiz=math.sqrt(number2)
 print(raiz)
+"""
 
-#Ejercicio 3.
+#Ejercicio 3. Write a program that receives from the user the radious of a circule. Your program should calculate the are of the circle.
+radiuos=float(input("Escriba el radio de su circulo: "))
+pi=math.pi
+areCircule=(pi*(radiuos**2))
+print("El área del circulo es: {}".format(areCircule))
+
+#Ejercicio 4. Write a program that calculates the result of this math expression y=x^2-3thanh(x)
+x=float(input("Digite el valor de X: "))
+y=(x**2)-(3*math.tanh(x))
+print("El resultado es: ", y)
