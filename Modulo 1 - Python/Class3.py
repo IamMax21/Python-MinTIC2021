18dc0c71b6e1647fde715b8b40472467dcb54e0a
Portatil Max <maxjip@gmail.com>
2021-06-03 15:07:58 -0500

    Anexamos el reto de la semana 1 y su solución - Corregido

--- Modulo 1 - Python/Class3.py
+++ Modulo 1 - Python/Class3.py
@@ -1,7 +1,8 @@
-import math
+from math import pow
 #Reto Semana 1. Escriba un programa que calcule el cuadrado de un número.
 
-number=float(input("Digite un número: "))
-pownumber=math.pow(number, 2)
+number=float(input())
+powNumber=pow(number, 2)
+print(powNumber)
 
-print(pownumber)
+
