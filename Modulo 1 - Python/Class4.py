81c58ff38314a2561e52f86b588a64f303d9cfc3
Max Jimenez <maxjip@gmail.com>
2021-06-04 00:19:18 -0500

    Operadores Lógicos en Python

--- Modulo 1 - Python/Class4.py
+++ Modulo 1 - Python/Class4.py
@@ -16,3 +16,30 @@ def tangenteAngulo(angulo):
     print(f"La tangente del ángulo es: {result1}")
 
 tangenteAngulo(5)
+
+
+#Operadores Logicos
+
+#AND. Solo es verdadero cuando todo es verdadero, en los demás casos es falso
+input1=True
+input2=True
+input3=False
+input4=False
+
+print(input1 and input2 and input3 and input4)
+
+#OR. Solo es falso cuanto todo es falso, en los demás casos es verdadero
+input1=True
+input2=True
+input3=False
+input4=False
+
+print(input1 or input2 or input3 or input4)
+
+#NOT. Cambia el estado de la operación lógica realizada
+input1=True
+input2=True
+input3=False
+input4=False
+
+print(not(input1 or input2 or input3 or input4))
