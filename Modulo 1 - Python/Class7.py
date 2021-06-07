32cb371f379fd72ec699176614aba1a167b20127
Portatil Max <maxjip@gmail.com>
2021-06-07 18:01:42 -0500

    Creamos un algoritmo para detectar que un numero es divisible entre 5

--- Modulo 1 - Python/Class7.py
+++ Modulo 1 - Python/Class7.py
@@ -9,3 +9,14 @@ else:
 
 #Algoritmo 2. Detectar que el numero es divisible entre 5
 
+number=float(input("Digite un número divisible entre 5: "))
+
+if number%5==0:
+    print("El número es divisible entre 5")
+else:
+    print("El número no es divisible entre 5")
+
+#Metodos
+"""
+Modulo. Para sacar el modulo de una division en pytho se hace mediante el caracter porcentual "%"
+Con esto podemos saber cuando un numero es divisible completamente por otro.
